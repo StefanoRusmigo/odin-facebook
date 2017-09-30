e382e8b625a1dce033e8abe569ad08aba0f3a092
Stefano Rusmigo <stefanorusmigo@hotmail.com>
2017-09-30 18:26:22 +0300

    sendgrid3

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,21 +1,9 @@
 class WelcomeMailer < ApplicationMailer
 
-require 'sendgrid'
-include SendGrid
-
-
-	def welcome_email(user)
-  from = Email.new(email: 'test@example.com')
-subject = 'Hello World from the SendGrid Ruby Library!'
-to = Email.new(email: 'test@example.com')
-content = Content.new(type: 'text/plain', value: 'Hello, Email!')
-mail = Mail.new(from, subject, to, content)
-
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-response = sg.client.mail._('send').post(request_body: mail.to_json)
-puts response.status_code
-puts response.body
-puts response.headers
-
+	
+	 def welcome_email(user)
+    @user = user
+  	@url  = new_user_session_url
+    mail(to: @user.email, subject: 'Welcome to Odin Social App')
   end
 end
